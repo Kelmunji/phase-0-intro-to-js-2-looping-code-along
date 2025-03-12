4c3d421534a61707aa92ee588786f02c87a8f4a9
kelvin Githinji <githinjikelvin74@gmail.com>
2025-03-12 16:29:40 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names) {
+    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+function countDown(n) {
+    while (n>= 0) {
+        console.log(n);
+        n--;
+    }
+
+}
